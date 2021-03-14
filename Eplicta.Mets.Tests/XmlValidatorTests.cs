efdee27fda68429be77e25f7fb66a8eba69f70ed
Daniel Bohlin <daniel.bohlin@eplicta.se>
2021-03-14 20:17:35 +0100

    add simple verification test

---
+++ Eplicta.Mets.Tests/XmlValidatorTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml;
+using FluentAssertions;
+using Xunit;
+
+namespace Eplicta.Mets.Tests
+{
+    public class XmlValidatorTests
+    {
+        [Fact]
+        public void Basic()
+        {
+            //Arrange
+            var document = new XmlDocument();
+            document.Load(@"C:\dev\Eplicta\mets\Resources\sample.xml");
+            //var root = document.CreateElement("root");
+            //var nsAttribute = document.CreateAttribute("xmlns", "xx", "http://www.w3.org/2000/xmlns/");
+            //nsAttribute.Value = "AAA";
+            //root.Attributes.Append(nsAttribute);
+
+            var schema = new XmlDocument();
+            schema.Load(@"C:\dev\Eplicta\mets\Resources\sample.xsd");
+
+            var sut = new XmlValidator();
+
+            //Act
+            var result = sut.Validate(document, schema);
+
+            //Assert
+            result.Should().BeEmpty();
+        }
+    }
+}
---
+++ Eplicta.Mets/XmlValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Eplicta.Mets
+{
+    public class XmlValidator
+    {
+        public IEnumerable<string> Validate(XmlDocument document, XmlDocument schema)
+        {
+            if (document == null) throw new ArgumentNullException(nameof(document));
+
+            var s = new XmlSchemaSet();
+            using var schemaStream = new StringReader(schema.OuterXml);
+            var xmlns = document.FirstChild?.Attributes?["xmlns"];
+            s.Add(xmlns?.InnerText, XmlReader.Create(schemaStream));
+
+            using var documentStream = new StringReader(document.OuterXml);
+            var rd = XmlReader.Create(documentStream);
+            var doc = XDocument.Load(rd);
+            doc.Validate(s, ValidationEventHandler);
+
+            yield break;
+        }
+
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            XmlSeverityType type = XmlSeverityType.Warning;
+            if (Enum.TryParse<XmlSeverityType>("Error", out type))
+            {
+                if (type == XmlSeverityType.Error) throw new Exception(e.Message);
+            }
+        }
+    }
+}
