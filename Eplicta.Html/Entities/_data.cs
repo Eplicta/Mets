f985f6aa4ab6e1ab39e0146ad41a1f29c908f8cb
Lawe02 <lawza02@gmail.com>
2022-04-06 15:14:14 +0200

    fixed problem with value in renderer

--- Eplicta.Html/Entities/_data.cs
+++ Eplicta.Html/Entities/_data.cs
@@ -5,11 +5,8 @@ namespace Eplicta.Html.Entities
     public record HtmlData
     {
         public Dictionary<string, string> Data { get; set; }
-
-        public Dictionary<string, string> Anka { get; set; }
-
-        public Dictionary<string, string> Fisk { get; set; } 
-
         public List<Dictionary<string, string>> Recourses { get; set; }
+ 
     }
+     
 }
