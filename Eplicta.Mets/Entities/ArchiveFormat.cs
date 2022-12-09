dc854ac5ea612db93566f39c228e2040ed3a4880
James Frih <james.frih@gruppera.se>
2022-12-09 11:59:22 +0100

    add tar to archive format list

--- Eplicta.Mets/Entities/ArchiveFormat.cs
+++ Eplicta.Mets/Entities/ArchiveFormat.cs
@@ -3,6 +3,7 @@
 public class ArchiveFormat : Enumeration<ArchiveFormat, ArchiveFormat>, IEnumerationItem
 {
     public static ArchiveFormat Zip = new("Zip");
+    public static ArchiveFormat Tar = new("Tar");
 
     public ArchiveFormat(string name)
     {
