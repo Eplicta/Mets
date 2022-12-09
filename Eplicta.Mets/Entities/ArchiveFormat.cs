953f1a5a760414a1190849c273ef58d1244e4a81
James Frih <james.frih@gruppera.se>
2022-12-09 11:58:01 +0100

    add ArchiveFormat

---
+++ Eplicta.Mets/Entities/ArchiveFormat.cs
@@ -0,0 +1,14 @@
+﻿namespace Eplicta.Mets.Entities;
+
+public class ArchiveFormat : Enumeration<ArchiveFormat, ArchiveFormat>, IEnumerationItem
+{
+    public static ArchiveFormat Zip = new("Zip");
+
+    public ArchiveFormat(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+}
